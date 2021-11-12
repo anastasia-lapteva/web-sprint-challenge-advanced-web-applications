421b9df4e926dd4b7f979bb916cb1c9daea037c3
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-11-12 08:55:51 -0800

    updated View

--- src/components/View.js
+++ src/components/View.js
@@ -3,46 +3,77 @@ import styled from 'styled-components';
 
 import Article from './Article';
 import EditForm from './EditForm';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const View = (props) => {
+const View = (props) =>
+{
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
 
-    const handleDelete = (id) => {
-    }
+    useEffect(() =>
+    {
+        axiosWithAuth()
+            .get("http://localhost:5000/api/articles")
+            .then((res) => 
+            {
+                setArticles(res.data);
+            })
+            .catch((err) => 
+            {
+                console.log({ err });
+            });
+    }, []);
+    const handleDelete = (id) =>
+    {
+        axiosWithAuth()
+            .delete(`http://localhost:5000/api/articles/${id}`)
+            .then((res) =>
+            {
+                setArticles(res.data);
+            })
+            .catch((err) =>
+            {
+                console.log({ err });
+            });
+    };
 
-    const handleEdit = (article) => {
-    }
+    const handleEdit = () =>
+    {
+        setEditing(false);
+    };
 
-    const handleEditSelect = (id)=> {
+    const handleEditSelect = (id) =>
+    {
         setEditing(true);
         setEditId(id);
-    }
+    };
 
-    const handleEditCancel = ()=>{
+    const handleEditCancel = () =>
+    {
         setEditing(false);
-    }
+    };
 
-    return(<ComponentContainer>
+    return (<ComponentContainer>
         <HeaderContainer>View Articles</HeaderContainer>
         <ContentContainer flexDirection="row">
             <ArticleContainer>
                 {
-                    articles.map(article => {
+                    articles.map(article =>
+                    {
                         return <ArticleDivider key={article.id}>
-                            <Article key={article.id} article={article} handleDelete={handleDelete} handleEditSelect={handleEditSelect}/>
-                        </ArticleDivider>
+                            <Article key={article.id} article={article} handleDelete={handleDelete} handleEditSelect={handleEditSelect} />
+                        </ArticleDivider>;
                     })
                 }
             </ArticleContainer>
-            
+
             {
-                editing && <EditForm editId={editId} handleEdit={handleEdit} handleEditCancel={handleEditCancel}/>
+                editing && <EditForm editId={editId} handleEdit={handleEdit} handleEditCancel={handleEditCancel} setArticles={setArticles} />
             }
         </ContentContainer>
     </ComponentContainer>);
-}
+};
 
 export default View;
 
@@ -55,7 +86,7 @@ export default View;
 
 const Container = styled.div`
     padding: 0.5em;
-`
+`;
 const HeaderContainer = styled.h1`
     border-bottom: solid black 2px;
     padding: 1em;
@@ -63,12 +94,12 @@ const HeaderContainer = styled.h1`
     font-size: 1.5em;
     background: black;
     color: white;
-`
+`;
 
 const ArticleDivider = styled.div`
     border-bottom: 1px solid black;
     padding: 1em;
-`
+`;
 
 const ComponentContainer = styled.div`
     display:flex;
@@ -76,12 +107,12 @@ const ComponentContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     
-`
+`;
 
 const ContentContainer = styled.div`
     display: flex;
     flex-direction: ${props => props.flexDirection};
-`
+`;
 
 const ArticleContainer = styled.div`
     background: grey;
