17a760ed4b0a44f12d424506133ae6fade6e15f9
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-11-12 08:49:20 -0800

    updated PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,21 @@
+import { rest } from "msw";
 import React from "react";
 import { Route } from "react-router-dom";
 
-function PrivateRoute() {
-  return (<Route/>);
-}
+const PrivateRoute = ({ component: Component, ...rest }) =>
+{
+    return <Route {...rest} render={(props) => 
+    {
+        if (localStorage.getItem("token"))
+        {
+            return <Component {...props} />;
+        }
+        else
+        {
+            return <Redirect to="/login" />;
+        }
+    }} />;
+};
 
 export default PrivateRoute;
 
