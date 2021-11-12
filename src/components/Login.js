b2a4cd6622a94dd5f73c542c2c5a1838b70c6c61
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-11-12 08:53:03 -0800

    updated Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,81 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { createBrowserHistory } from "history";
 
-const Login = () => {
-    
-    return(<ComponentContainer>
-        <ModalContainer>
-            <h1>Welcome to Blogger Pro</h1>
-            <h2>Please enter your account information.</h2>
-        </ModalContainer>
-    </ComponentContainer>);
-}
+const initialValues =
+{
+    username: "",
+    password: ""
+};
+
+const Login = (props) =>
+{
+    const history = createBrowserHistory({ forceRefresh: true });
+    const [formValues, setFormValues] = useState(initialValues);
+    const [error, setError] = useState("");
+
+    const handleChanges = (e) =>
+    {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value });
+        if (formValues.username === "" || formValues.password === "")
+        {
+            setError("All fields must be filled out.");
+        }
+        else 
+        {
+            setError("");
+        }
+    };
+
+    const handleSubmit = (e) =>
+    {
+        e.preventDefault();
+        axios.post("http://localhost:5000/api/login", formValues)
+            .then((res) => 
+            {
+                // console.log(res);
+                // set res.data.token to localStorage
+                // route user to the home page(/view)
+                localStorage.setItem("token", res.data.token);
+                history.push("/view");
+            })
+            .catch((err) => 
+            {
+                console.log(err);
+                setError(err.toString());
+            });
+    };
+
+    return (
+        <ComponentContainer>
+            <ModalContainer>
+                <h1>Welcome to Blogger Pro</h1>
+                <h2>Please enter your account information.</h2>
+                {error && <p style={{ color: 'red' }} > {error}</p>}
+
+                <FormGroup onSubmit={handleSubmit}>
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                        id="username"
+                        name="username"
+                        value={formValues.username}
+                        onChange={handleChanges}
+                    />
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={formValues.password}
+                        onChange={handleChanges}
+                    />
+                    <Button>Login</Button>
+                </FormGroup>
+            </ModalContainer>
+        </ComponentContainer >);
+};
 
 export default Login;
 
@@ -26,32 +92,33 @@ const ComponentContainer = styled.div`
     justify-content: center;
     align-items: center;
     display:flex;
-`
+`;
 
 const ModalContainer = styled.div`
     width: 500px;
     background: white;
     padding: 2rem;
     text-align: center;
-`
+`;
 
 const Label = styled.label`
     display: block;
     text-align: left;
     font-size: 1.5rem;
-`
+`;
 
 const FormGroup = styled.form`
     padding:1rem;
-`
+`;
 
 const Input = styled.input`
     font-size: 1rem;
     padding: 1rem 0;
     width:100%;
-`
+`;
 
 const Button = styled.button`
     padding:1rem;
     width: 100%;
-`
+    background: linear-gradient(to bottom, #33ccff 0%, #ff99cc 100%);
+`;
